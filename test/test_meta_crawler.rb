db9f34cee1a12fa9c8a9284e61c65f812cfa015b
Josh Lindsey <joshua.s.lindsey@gmail.com>
2010-04-08 14:22:46 -0400

    Updated test suite to reflect the new methodology. All tests pass.

--- test/test_meta_crawler.rb
+++ test/test_meta_crawler.rb
@@ -1,71 +1,70 @@
 require 'helper'
 
 class TestMetaCrawler < Test::Unit::TestCase
-  context "The Meta crawler class" do
+  context "The Meta crawler" do
     should "respond to crawl" do
-      assert_respond_to Scapeshift::Crawlers::Meta, :crawl
+      assert_respond_to Scapeshift::Crawlers::Meta.new(:type => "fake"), :crawl
     end
 
-    context "The Meta crawler" do
-      context "when passed no options" do
-        should "raise the appropriate exception" do
-          assert_raise Scapeshift::Errors::InsufficientOptions do
-            Scapeshift::Crawlers::Meta.crawl {}
-          end
+    context "when passed no options" do
+      should "raise the appropriate exception" do
+        assert_raise Scapeshift::Errors::InsufficientOptions do
+          Scapeshift::Crawlers::Meta.new
         end
       end
+    end
 
-      context "when passed a bad meta type" do
-        should "raise the appropiate exception" do
-          assert_raise Scapeshift::Errors::UnknownMetaType do
-            Scapeshift::Crawlers::Meta.crawl :type => :not_a_real_type
-          end
+    context "when passed a bad meta type" do
+      should "raise the appropiate exception" do
+        assert_raise Scapeshift::Errors::UnknownMetaType do
+          crawler = Scapeshift::Crawlers::Meta.new :type => :not_a_real_type
+          crawler.crawl
         end
       end
+    end
 
-      context "when crawling expansion Sets" do
-        setup do
-          @sets = Scapeshift::Crawler.crawl :meta, :type => :sets
-        end
-
-        should "return a SortedSet" do
-          assert_instance_of SortedSet, @sets
-        end
-
-        should "return valid expansion sets" do
-          check = Set.new %w{Darksteel Coldsnap Zendikar Shadowmoor Lorwyn Nemesis Onslaught}
-          assert check.proper_subset?(@sets)
-        end
+    context "when crawling expansion Sets" do
+      setup do
+        @sets = Scapeshift::Crawler.crawl :meta, :type => :sets
       end
 
-      context "when crawling Formats" do
-        setup do
-          @formats = Scapeshift::Crawler.crawl :meta, :type => :formats
-        end
-
-        should "return a SortedSet" do
-          assert_instance_of SortedSet, @formats
-        end
-
-        should "return valid formats" do
-          check = Set.new %w{Standard Classic Legacy Extended Freeform Vintage}
-          assert check.proper_subset?(@formats)
-        end
+      should "return a SortedSet" do
+        assert_instance_of SortedSet, @sets
       end
 
-      context "when crawling Card Types" do
-        setup do
-          @types = Scapeshift::Crawler.crawl :meta, :type => :types
-        end
+      should "return valid expansion sets" do
+        check = Set.new %w{Darksteel Coldsnap Zendikar Shadowmoor Lorwyn Nemesis Onslaught}
+        assert check.proper_subset?(@sets)
+      end
+    end
 
-        should "return a SortedSet" do
-          assert_instance_of SortedSet, @types
-        end
+    context "when crawling Formats" do
+      setup do
+        @formats = Scapeshift::Crawler.crawl :meta, :type => :formats
+      end
 
-        should "return valid card types" do
-          check = Set.new %w{Artifact World Tribal Plane Land}
-          assert check.proper_subset?(@types)
-        end
+      should "return a SortedSet" do
+        assert_instance_of SortedSet, @formats
+      end
+
+      should "return valid formats" do
+        check = Set.new %w{Standard Classic Legacy Extended Freeform Vintage}
+        assert check.proper_subset?(@formats)
+      end
+    end
+
+    context "when crawling Card Types" do
+      setup do
+        @types = Scapeshift::Crawler.crawl :meta, :type => :types
+      end
+
+      should "return a SortedSet" do
+        assert_instance_of SortedSet, @types
+      end
+
+      should "return valid card types" do
+        check = Set.new %w{Artifact World Tribal Plane Land}
+        assert check.proper_subset?(@types)
       end
     end
   end
